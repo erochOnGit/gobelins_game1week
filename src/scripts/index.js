38a718c327323616732e4328df494f78cad1e613
emile <emile31.roch@gmail.com>
2020-12-02 10:35:58 +0100

    Update data

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,30 +1,60 @@
 import "../styles/index.scss";
-
+const Game = require("./Game/Game");
 if (process.env.NODE_ENV === "development") {
   require("../index.html");
 }
+
 //gonna go in a file.json
 const data = {
   sentences: [
     {
       words: [
-        {
-          content: "bonjour",
-          picurl: "",
-          similarWords: ["bonne", "contour", "jour"],
-        },
-        { content: "je", picurl: "", similarWords: ["tu", "j'y", "jus"] },
-        {
-          content: "voudrai",
-          picurl: "",
-          similarWords: ["hourra", "prendrai", "mangerai"],
-        },
-        { content: "du", picurl: "", similarWords: ["dis", "dans", "de"] },
-        {
-          content: "pain",
-          picurl: "",
-          similarWords: ["pan", "pour", "pendre"],
-        },
+        [
+          {
+            content: "bonjour",
+            picurl: "",
+            value: 5,
+          },
+          {
+            content: "bonne",
+            picurl: "",
+            value: 4,
+          },
+          {
+            content: "contour",
+            picurl: "",
+            value: 3,
+          },
+          {
+            content: "jour",
+            picurl: "",
+            value: 2,
+          },
+        ],
+        [
+          { content: "je", picurl: "", value: 5 },
+          { content: "tu", picurl: "", value: 4 },
+          { content: "j'y", picurl: "", value: 3 },
+          { content: "jus", picurl: "", value: 2 },
+        ],
+        [
+          { content: "voudrai", picurl: "", value: 5 },
+          { content: "prendrai", picurl: "", value: 4 },
+          { content: "mangerai", picurl: "", value: 3 },
+          { content: "hourra", picurl: "", value: 2 },
+        ],
+        [
+          { content: "du", picurl: "", value: 5 },
+          { content: "dis", picurl: "", value: 4 },
+          { content: "dans", picurl: "", value: 3 },
+          { content: "de", picurl: "", value: 2 },
+        ],
+        [
+          { content: "pain", picurl: "", value: 5 },
+          { content: "pan", picurl: "", value: 4 },
+          { content: "pour", picurl: "", value: 3 },
+          { content: "pendre", picurl: "", value: 2 },
+        ],
       ],
     },
   ],
